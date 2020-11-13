1ba722ab2a1402d00ee8f8c248243728ecba25ca
shiwh <shi_wenhu@qq.com>
2020-11-13 16:14:06 +0800

    group controller

--- goft/Goft.go
+++ goft/Goft.go
@@ -4,6 +4,7 @@ import "github.com/gin-gonic/gin"
 
 type Goft struct {
 	*gin.Engine
+	g *gin.RouterGroup
 }
 
 // 初始化
@@ -14,7 +15,15 @@ func Ignite() *Goft {
 	return g
 }
 
-func (goft *Goft) Mount(controllers ...IController) *Goft {
+// 重写gin.Handle方法，这里可以直接加载group，省略在控制器中还要加这个参数
+func (goft *Goft) Handle(httpMethod, relativePath string, handlers ...gin.HandlerFunc) *Goft {
+	goft.g.Handle(httpMethod, relativePath, handlers...)
+	return goft
+}
+
+// 挂在控制器
+func (goft *Goft) Mount(group string, controllers ...IController) *Goft {
+	goft.g = goft.Group(group)
 	for _, controller := range controllers {
 		controller.Build(goft)
 	}
@@ -23,6 +32,5 @@ func (goft *Goft) Mount(controllers ...IController) *Goft {
 
 // 启动程序
 func (goft *Goft) Launch() {
-
 	goft.Run(":8081")
 }
--- tests/test_main.go
+++ tests/test_main.go
@@ -7,6 +7,11 @@ import (
 
 func main() {
 	goft.Ignite().
-		Mount(NewIndexController()).
+		Mount("v1", NewIndexController()).
+		Mount("v2", NewIndexController()).
 		Launch()
+
+	//g := gin.New()
+	//r := g.Group()
+	//r.Handle()
 }
